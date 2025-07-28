921836cbf25c4a09b94ee185ada9500582e5b1c8
mohamedayari1 <ayarim781@gmail.com>
2025-07-29 00:53:21 +0100

    fix the text area styling

--- components/minimal-components/chat.tsx
+++ components/minimal-components/chat.tsx
@@ -5,7 +5,9 @@ export function Chat() {
   return (
     <main>
       <Messages />
-      <Input />
+        <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+        <Input />
+      </form>
     </main>
   );
 }
--- components/minimal-components/input.tsx
+++ components/minimal-components/input.tsx
@@ -1,24 +1,29 @@
+import cx from "classnames";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
-import cx from 'classnames';
-
-
+import { ArrowUpIcon } from "../icons";
 
 export default function Input() {
   return (
-    <div>
+    <div className="relative w-full flex flex-col gap-4">
       <Textarea
         className={cx(
           "min-h-[24px] max-h-[calc(75dvh)] overflow-hidden resize-none rounded-2xl !text-base bg-muted pb-10",
         )}
+        placeholder="Send a message..."
       />
 
-        <div>
-            <Button>
-                Send
-            </Button>
-        </div>
+        <SendButton />
+    </div>
+  );
+}
 
+function SendButton() {
+  return (
+    <div className="absolute bottom-0 right-0 p-2">
+      <Button className="rounded-full p-1.5 h-fit border">
+        <ArrowUpIcon size={16} />
+      </Button>
     </div>
   );
 }
