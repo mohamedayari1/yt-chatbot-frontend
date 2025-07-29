efbb4781f1b0f15fd607dfd841f016175de40f4e
mohamedayari1 <ayarim781@gmail.com>
2025-07-29 23:42:56 +0100

    add basic shadcn sidebar components

--- components/minimal-components/app-sidebar.tsx
+++ components/minimal-components/app-sidebar.tsx
@@ -1,21 +1,69 @@
-'use client';
+"use client";
+import { useRouter } from "next/navigation";
 
-
+import { PlusIcon } from "@/components/icons";
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
+  SidebarContent,
+  SidebarFooter,
   SidebarHeader,
   SidebarMenu,
   useSidebar,
 } from "@/components/ui/sidebar";
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
 export default function AppSidebar() {
   const { setOpenMobile } = useSidebar();
+  const router = useRouter();
 
   return (
-    <Sidebar>
+    <Sidebar className="group-data-[side=left]:border-r-0">
       <SidebarHeader>
-        <SidebarMenu>sdfqsdf</SidebarMenu>
+        <SidebarMenu>
+          <div className="flex flex-row justify-between items-center">
+            <Link
+              href="/"
+              onClick={() => {
+                setOpenMobile(false);
+              }}
+              className="flex flex-row gap-3 items-center"
+            >
+              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
+                Chatbot
+              </span>
+            </Link>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  type="button"
+                  className="p-2 h-fit"
+                  onClick={() => {
+                    setOpenMobile(false);
+                    router.push("/");
+                    router.refresh();
+                  }}
+                >
+                  <PlusIcon />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent align="end">New Chat</TooltipContent>
+            </Tooltip>
+          </div>
+        </SidebarMenu>
       </SidebarHeader>
+
+      <SidebarContent>
+        <div>catalunya</div>
+      </SidebarContent>
+      <SidebarFooter>
+        <div>catalunya</div>
+      </SidebarFooter>
     </Sidebar>
   );
 }
